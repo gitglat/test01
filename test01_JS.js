4351473a8933aa50acc769614b1f0045ddd64064
Glatt <moscowglatt@gmail.com>
2019-07-05 13:59:19 +0300

    это первый комит

---
+++ test01_JS.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//var x=1;
+//console.log(x);
+// alert("hello");
+//let ansver = confirm("Hey gay");
+//console.log(ansver);
+//let ansver = prompt("Hey gay?", "Da");
+//console.log(ansver);
+
+let incr = 10,
+decr = 10;
+
+
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
